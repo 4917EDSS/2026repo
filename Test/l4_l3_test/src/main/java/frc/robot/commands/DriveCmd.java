f1fe697a422297d4e24a0eb8e7a5ab0133bbc5bf
CarsonT0192 <car_jets@icloud.com>
2026-01-16 18:28:08 -0500

    fixed networktable issues

--- Test/l4_l3_test/src/main/java/frc/robot/commands/DriveCmd.java
+++ Test/l4_l3_test/src/main/java/frc/robot/commands/DriveCmd.java
@@ -6,10 +6,15 @@ package frc.robot.commands;
 
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.kinematics.Odometry;
+import edu.wpi.first.networktables.DoubleArraySubscriber;
+import edu.wpi.first.networktables.DoubleSubscriber;
+import edu.wpi.first.networktables.GenericEntry;
+import edu.wpi.first.networktables.GenericSubscriber;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.networktables.StructPublisher;
+import edu.wpi.first.networktables.StructSubscriber;
 import edu.wpi.first.networktables.StructTopic;
 import edu.wpi.first.units.measure.Distance;
 import edu.wpi.first.wpilibj2.command.Command;
@@ -25,8 +30,9 @@ public class DriveCmd extends Command {
   private final SwerveRequest.RobotCentric testDrive = new SwerveRequest.RobotCentric().withDriveRequestType(DriveRequestType.Velocity);
   DrivetrainSub m_drivetrainSub;
   double distanceToDrive;
-  NetworkTableEntry currentPoseEntry;
-  Pose2d currentPose;
+  DoubleArraySubscriber poseSubscriber;
+  double[] startingPoseArray;
+  double[] currentPoseArray;
 
   /** Creates a new driveCmd. */
   public DriveCmd(DrivetrainSub drivetrainSub) {
@@ -38,7 +44,8 @@ public class DriveCmd extends Command {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    currentPoseEntry = table.getEntry("pose");
+    poseSubscriber = table.getDoubleArrayTopic("robotPose").subscribe(null, null);
+    startingPoseArray = poseSubscriber.get();
     distanceToDrive = 15; //Subject to change
     m_drivetrainSub.setControl(testDrive.withVelocityX(99999999).withVelocityY(0).withRotationalRate(0));
   }
@@ -46,7 +53,7 @@ public class DriveCmd extends Command {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    //currentPose2d.get
+    currentPoseArray = startingPoseArray = poseSubscriber.get();
   }
 
   // Called once the command ends or is interrupted.
@@ -58,7 +65,9 @@ public class DriveCmd extends Command {
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    
+    if(currentPoseArray[0]-startingPoseArray[0]<15){
+      return true;
+    }
     return false;
   }
 }
