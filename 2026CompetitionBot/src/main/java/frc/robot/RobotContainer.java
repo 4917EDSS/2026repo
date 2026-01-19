598d9ac1054505b8a317d3697874aa259dd5ea9b
4917 Juliette <software@4917.ca>
2026-01-19 16:37:52 -0500

    improved intake sub and finished defending logic

--- 2026CompetitionBot/src/main/java/frc/robot/RobotContainer.java
+++ 2026CompetitionBot/src/main/java/frc/robot/RobotContainer.java
@@ -9,6 +9,7 @@ import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.StartEndCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.Constants.OperatorConstants;
@@ -46,7 +47,7 @@ public class RobotContainer {
 
   private defendFirst m_amIDefendingFirst = defendFirst.UNKNOWN;
 
-  private final CommandXboxController m_diverContoller =
+  private final CommandXboxController m_driverController =
       new CommandXboxController(Constants.OperatorConstants.kDriverControllerPort);
   private final CommandXboxController m_operatorContoller =
       new CommandXboxController(Constants.OperatorConstants.kOperatorControllerPort);
@@ -80,14 +81,16 @@ public class RobotContainer {
     // and Y is defined as to the left according to WPILib convention.
     m_drivetrainSub.setDefaultCommand(
         // Drivetrain will execute this command periodically
-        m_drivetrainSub.applyRequest(() -> drive.withVelocityX(-m_diverContoller.getLeftY() * MaxSpeed) // Drive forward with negative Y (forward)
-            .withVelocityY(-m_diverContoller.getLeftX() * MaxSpeed) // Drive left with negative X (left)
-            .withRotationalRate(-m_diverContoller.getRightX() * MaxAngularRate) // Drive counterclockwise with negative X (left)
+        m_drivetrainSub.applyRequest(() -> drive.withVelocityX(-m_driverController.getLeftY() * MaxSpeed) // Drive forward with negative Y (forward)
+            .withVelocityY(-m_driverController.getLeftX() * MaxSpeed) // Drive left with negative X (left)
+            .withRotationalRate(-m_driverController.getRightX() * MaxAngularRate) // Drive counterclockwise with negative X (left)
         ));
 
 
     // Reset the field-centric heading on left bumper press.
-    m_diverContoller.back().onTrue(m_drivetrainSub.runOnce(m_drivetrainSub::seedFieldCentric));
+    m_driverController.back().onTrue(m_drivetrainSub.runOnce(m_drivetrainSub::seedFieldCentric));
+
+    m_driverController.a().whileTrue(new StartEndCommand(() -> m_intakeSub.setIntakePower(1.0), () -> m_intakeSub.setIntakePower(0.0)));
 
   }
 
@@ -136,17 +139,6 @@ public class RobotContainer {
     }
     return "unknown";
   }
-
-  //canScoreNow
-  //chcek if enum is unknown (if unknown process) 
-  //check if unknown again
-  //if not unknown check match timer
-
-  //find shift
-  //if shift matches with game data and if 
-
-  //130, 105, 80, 55, 30
-
 
 
   public void processGameData() {
