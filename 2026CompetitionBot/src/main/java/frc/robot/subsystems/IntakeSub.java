c24f7d71c67a08a71fc2248e8bef747d681ed2e7
4917 Juliette <software@4917.ca>
2026-01-19 19:50:46 -0500

    adjustd intake motors as neos

--- 2026CompetitionBot/src/main/java/frc/robot/subsystems/IntakeSub.java
+++ 2026CompetitionBot/src/main/java/frc/robot/subsystems/IntakeSub.java
@@ -11,10 +11,19 @@ import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.hardware.TalonFX;
 import frc.robot.Constants;
 
+import com.revrobotics.spark.SparkBase;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import com.revrobotics.spark.config.SparkMaxConfig;
+
 public class IntakeSub extends SubsystemBase {
   private static Logger m_logger = Logger.getLogger(IntakeSub.class.getName());
   private boolean m_intakeison = false;
-  private final TalonFX m_IntakeMotor = new TalonFX(Constants.CanIds.kIntakeMotor); // To be changed later
+  //private final TalonFX m_IntakeMotor = new TalonFX(Constants.CanIds.kIntakeMotor); // To be changed later
+
+  private final SparkMax m_intakeMotor = new SparkMax(3, MotorType.kBrushless);
+
   private final TalonFX m_IntakeArmMotor = new TalonFX(Constants.CanIds.kIntakeArmMotor);
   private double m_ArmPower = 0;
   
@@ -34,7 +43,18 @@ public class IntakeSub extends SubsystemBase {
     config.SoftwareLimitSwitch.ForwardSoftLimitEnable = true;
 
     // Apply to the m_IntakeArmMotor
-    m_IntakeArmMotor.getConfigurator().apply(config);
+    //m_IntakeArmMotor.getConfigurator().apply(config);
+
+
+
+    SparkMaxConfig motorConfig = new SparkMaxConfig();
+    motorConfig
+        .inverted(true) // Set to true to invert the forward motor direction
+        .smartCurrentLimit(60) // Current limit in amps
+        .idleMode(IdleMode.kBrake);
+
+    m_intakeMotor.configure(motorConfig, SparkBase.ResetMode.kResetSafeParameters,
+        SparkBase.PersistMode.kPersistParameters);
   }
 
 
@@ -50,7 +70,7 @@ public class IntakeSub extends SubsystemBase {
   }
 
   public void setIntakePower(double power) {
-    m_IntakeMotor.set(power);
+    m_intakeMotor.set(power);
   }
 
   public void setIntakeArmPower(double power) {
