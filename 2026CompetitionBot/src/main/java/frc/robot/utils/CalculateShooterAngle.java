a2172c2630b4a755573721015d2a5b37ecd5cf43
4917 Ultron <software@4917.ca>
2026-01-24 10:09:13 -0500

    Adjusted shooter position in relation to robot

--- 2026CompetitionBot/src/main/java/frc/robot/utils/CalculateShooterAngle.java
+++ 2026CompetitionBot/src/main/java/frc/robot/utils/CalculateShooterAngle.java
@@ -4,14 +4,23 @@
 
 package frc.robot.utils;
 
-import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.geometry.Transform3d;
+import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import frc.robot.Constants;
 
 /** Add your docs here. */
 public class CalculateShooterAngle {
-  public double getAngle(Pose2d robot) {
+  public double getAngle(Pose3d robot) {
     Alliance alliance = GameData.getAlliance();
+
+    Pose3d shooterPoseinfield = new Pose3d();
+    Transform3d shooterToRobot = new Transform3d(
+        new Translation3d(0.3, 0.0, 0.3),
+        new Rotation3d(0.0, 0.0, 0.0));
+    shooterPoseinfield = componentToField(shooterPoseinfield, robot, shooterToRobot);
 
     double distX = 1; // default values
     double distY = 0;
@@ -28,11 +37,26 @@ public class CalculateShooterAngle {
     double angle = Math.toDegrees(Math.atan2(distY, distX));
 
     // get the robot's angle
-    double heading = robot.getRotation().getDegrees();
+    double heading = shooterPoseinfield.toPose2d().getRotation().getDegrees();
 
     // subtract the robot heading from the shooter angle
     angle -= heading;
 
     return angle;
   }
+
+  private static Pose3d componentToField(Pose3d componentRelativePose, Pose3d robotPose,
+      Transform3d componentToRobotTransform) {
+    Pose3d robotRelative = componentToRobot(componentRelativePose, componentToRobotTransform);
+    return robotToField(robotRelative, robotPose);
+  }
+
+  private static Pose3d componentToRobot(Pose3d componentRelativePose, Transform3d componentToRobotTransform) {
+    return componentRelativePose.plus(componentToRobotTransform);
+  }
+
+  private static Pose3d robotToField(Pose3d robotRelativePose, Pose3d robotPose) {
+    return robotRelativePose.relativeTo(robotPose);
+  }
+
 }
