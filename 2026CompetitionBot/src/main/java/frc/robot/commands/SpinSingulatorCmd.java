565e0d6503336f50af310a97fc40829c81b408fe
4917 Golf <software@4917.ca>
2026-02-07 14:30:03 -0500

    Cleaned up SpinSingulatorCmd and HopperSub
    Also changed escalator motor to a Sparkmax

--- 2026CompetitionBot/src/main/java/frc/robot/commands/SpinSingulatorCmd.java
+++ 2026CompetitionBot/src/main/java/frc/robot/commands/SpinSingulatorCmd.java
@@ -8,12 +8,16 @@ import edu.wpi.first.units.measure.Velocity;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.HopperSub;
 
-/* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
-public class SpinSinglatorCmd extends Command {
+/*
+ * You should consider using the more terse Command factories API instead
+ * https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands
+ */
+public class SpinSingulatorCmd extends Command {
   HopperSub m_hopperSub;
   double m_velocity;
+
   /** Creates a new SpinSinglator. */
-  public SpinSinglatorCmd(HopperSub hopperSub, double velocity) {
+  public SpinSingulatorCmd(HopperSub hopperSub, double velocity) {
     m_hopperSub = hopperSub;
     m_velocity = velocity;
     addRequirements(m_hopperSub);
