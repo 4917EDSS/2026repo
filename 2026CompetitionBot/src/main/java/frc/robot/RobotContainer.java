6f1c954e313fe87280a906d29e11277bf7efbe3c
4917 Roomba <software+roomba@4917.ca>
2026-02-13 15:36:16 -0500

    add button a on the opperator controler

--- 2026CompetitionBot/src/main/java/frc/robot/RobotContainer.java
+++ 2026CompetitionBot/src/main/java/frc/robot/RobotContainer.java
@@ -16,6 +16,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.RunCommand;
+import edu.wpi.first.wpilibj2.command.StartEndCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.commands.ClimbCmd;
 import frc.robot.commands.DriveToPoseCmd;
@@ -150,6 +151,9 @@ public class RobotContainer {
 
     // Operator A
     // TODO: Run deploy motor with low positive power while held
+
+    m_operatorContoller.a().whileTrue(
+        new StartEndCommand(() -> m_intakeSub.setDeployPower(0.1), () -> m_intakeSub.setDeployPower(0.0), m_intakeSub));
 
     // Operator B
     // TODO: Run deploy motor with low negative power while held
