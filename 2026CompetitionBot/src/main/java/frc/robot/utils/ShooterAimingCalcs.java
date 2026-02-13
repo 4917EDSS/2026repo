6fdfe6abdd0c2c2ea7c44df74a1cc2bbbfeaec33
CarsonT0192 <car_jets@icloud.com>
2026-02-12 20:19:02 -0500

    Improved shooter aim calcs

--- 2026CompetitionBot/src/main/java/frc/robot/utils/ShooterAimingCalcs.java
+++ 2026CompetitionBot/src/main/java/frc/robot/utils/ShooterAimingCalcs.java
@@ -5,15 +5,36 @@
 package frc.robot.utils;
 
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Transform2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
+import frc.robot.Constants;
 
 /** Add your docs here. */
 public class ShooterAimingCalcs {
   private double targetAngle;
   private double robotAngle;
+  private Pose2d turretPos;
+  private Translation2d distToHub;
+  private double relativeTargetAngle;
 
   public double calculateShooterRotation(Pose2d robot) {
+    Alliance alliance = GameData.getAlliance();
+
+    turretPos = robot.plus(new Transform2d(-0.1,0.1, new Rotation2d(0.0)));
     robotAngle = robot.getRotation().getDegrees() + 180; //robot angle 0 to 360
-    targetAngle = 360 - robotAngle;
-    return targetAngle;
+
+    if (alliance == Alliance.Blue){
+      distToHub = new Translation2d(Constants.FieldElements.kBlueHubX-turretPos.getX(), Constants.FieldElements.kBlueHubY-turretPos.getY());
+    } else {
+      distToHub = new Translation2d(Constants.FieldElements.kRedHubX-turretPos.getX(), Constants.FieldElements.kRedHubY-turretPos.getY());
+    }
+
+    targetAngle = Math.toDegrees(Math.atan2(distToHub.getY(), distToHub.getX()))+180;
+
+    relativeTargetAngle = (360+targetAngle-robotAngle)%360;
+    
+    return relativeTargetAngle;
   }
 }
