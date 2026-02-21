bc99f651c5f2c2893a9b8a236de546b1cfeecf6d
4917 Golf <software@4917.ca>
2026-02-21 12:39:52 -0500

    Added time of flight and shooter in motion calculations

--- 2026CompetitionBot/src/main/java/frc/robot/utils/ShooterAimingCalcs.java
+++ 2026CompetitionBot/src/main/java/frc/robot/utils/ShooterAimingCalcs.java
@@ -15,8 +15,6 @@ import frc.robot.Constants;
 public class ShooterAimingCalcs {
   private double targetYawAngle;
   private double robotYawAngle;
-  private Pose2d turretPos;
-  private Translation2d distToHub;
   private double relativeTargetYawAngle;
 
   private double flywheelPower;
@@ -26,10 +24,35 @@ public class ShooterAimingCalcs {
   }
 
   public double calculateShooterYawDegrees(Pose2d robot) {
-    Alliance alliance = GameData.getAlliance();
-
-    turretPos = robot.plus(new Transform2d(-0.1, 0.1, new Rotation2d(0.0)));
     robotYawAngle = robot.getRotation().getDegrees() + 180; //robot angle 0 to 360
+
+    targetYawAngle =
+        Math.toDegrees(Math.atan2(getDistanceFromHub(robot).getY(), getDistanceFromHub(robot).getX())) + 180;
+
+    relativeTargetYawAngle = (360 + (targetYawAngle - robotYawAngle)) % 360;
+
+    return relativeTargetYawAngle;
+  }
+
+  public double calculateShooterYawInMotion(Pose2d robot, double velocityX, double velocityY, double angularVelocity,
+      double launchVelocity, double launchPitch) {
+    double offsetX = velocityX * calculateTimeOfFlight(robot, launchVelocity, launchPitch); // Get the offset by getting the product of the x velocity and the time of flight
+    double offsetY = velocityY * calculateTimeOfFlight(robot, launchVelocity, launchPitch); // Same with y
+    double offsetRot = angularVelocity * calculateTimeOfFlight(robot, launchVelocity, launchPitch); // Same but with angular velocity
+    return calculateShooterYawDegrees(new Pose2d(robot.getX() + offsetX, robot.getY() + offsetY,
+        robot.getRotation().plus(new Rotation2d(offsetRot)))); // Returns a new pose2d with the offset added
+  }
+
+  public double calculateTimeOfFlight(Pose2d robot, double launchVelocity, double launchPitch) {
+    double distanceX = getDistanceFromHub(robot).getX();
+    double velocityX = launchVelocity * Math.cos(launchPitch);
+    return distanceX / velocityX;
+  }
+
+  public Translation2d getDistanceFromHub(Pose2d robot) {
+    Alliance alliance = GameData.getAlliance();
+    Translation2d distToHub;
+    Pose2d turretPos = robot.plus(new Transform2d(-0.1, 0.1, new Rotation2d(0.0)));
 
     if(alliance == Alliance.Blue) {
       distToHub = new Translation2d(Constants.FieldElements.kBlueHubX - turretPos.getX(),
@@ -38,12 +61,7 @@ public class ShooterAimingCalcs {
       distToHub = new Translation2d(Constants.FieldElements.kRedHubX - turretPos.getX(),
           Constants.FieldElements.kRedHubY - turretPos.getY());
     }
-
-    targetYawAngle = Math.toDegrees(Math.atan2(distToHub.getY(), distToHub.getX())) + 180;
-
-    relativeTargetYawAngle = (360 + targetYawAngle - robotYawAngle) % 360;
-
-    return relativeTargetYawAngle;
+    return distToHub;
   }
 
   public double calculateShooterPitchDegrees() {
