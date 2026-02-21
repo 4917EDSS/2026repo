1f731c4f061104cdd04073e58a91581243a62e96
4917 Ultron <software@4917.ca>
2026-02-21 14:37:39 -0500

    shooter pitch sysid and positional control

--- 2026CompetitionBot/src/main/java/frc/robot/Constants.java
+++ 2026CompetitionBot/src/main/java/frc/robot/Constants.java
@@ -144,7 +144,10 @@ public final class Constants {
     public static final double kPitchMaxCurrent = 120.0;
     public static final double kPitchMaxPower = 0.10;
     public static final double kPitchTolerance = 2.0;
+    public static final double kPitchMaxVelocityDegPerSec = 60.0;
+    public static final double kPitchMaxAccelerationDegPerSec = 120.0;
     public static final double kPitchKS = 0.25;
+    public static final double kPitchKG = 0.0;
     public static final double kPitchKV = 0.12;
     public static final double kPitchKP = 0.1;
     public static final double kPitchKI = 0.0;
