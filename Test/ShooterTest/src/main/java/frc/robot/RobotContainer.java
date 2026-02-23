170c40295da5e13fa380021d720cc5cffbb2dd7d
4917 Ultron <software@4917.ca>
2026-02-23 17:11:53 -0500

    made the shooter track the hub as a default command (we think)

--- Test/ShooterTest/src/main/java/frc/robot/RobotContainer.java
+++ Test/ShooterTest/src/main/java/frc/robot/RobotContainer.java
@@ -60,8 +60,9 @@ public class RobotContainer {
             .withRotationalRate(-m_driverController.getRightX() * maxAngularRate) // Drive counterclockwise with negative X (left)
         ));
 
-    //m_shooterSub.setDefaultCommand(new RunCommand( ()-> m_shooterSub
-    //    .setTargetYawAngle(m_shooterAimingCalcs.calculateShooterYawDegrees(m_drivetrainSub.getState().Pose))));
+    m_shooterSub.setDefaultCommand(new RunCommand(() -> m_shooterSub
+        .setTargetYawAngle(m_shooterAimingCalcs.calculateShooterYawDegrees(m_drivetrainSub.getState().Pose)),
+        m_shooterSub));
 
     // Configure the trigger bindings
     configureBindings();
