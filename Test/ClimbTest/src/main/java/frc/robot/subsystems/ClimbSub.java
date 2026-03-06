909ee8abd53e88bb04fc17128c63c2f2ea1c21ca
4917-Baymax <software@4917.ca>
2026-03-06 16:31:06 -0500

    Created test project for climb

---
+++ Test/ClimbTest/src/main/java/frc/robot/subsystems/ClimbSub.java
@@ -0,0 +1,282 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import java.util.logging.Logger;
+import com.ctre.phoenix6.configs.CurrentLimitsConfigs;
+import com.ctre.phoenix6.configs.MotionMagicConfigs;
+import com.ctre.phoenix6.configs.MotorOutputConfigs;
+import com.ctre.phoenix6.configs.Slot0Configs;
+import com.ctre.phoenix6.configs.TalonFXConfigurator;
+import com.ctre.phoenix6.controls.DutyCycleOut;
+import com.ctre.phoenix6.controls.MotionMagicTorqueCurrentFOC;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.InvertedValue;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.units.Units;
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.RobotController;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.sysid.SysIdRoutineLog;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
+import frc.robot.Constants;
+
+
+public class ClimbSub extends SubsystemBase {
+  private static Logger m_logger = Logger.getLogger(ClimbSub.class.getName());
+
+  private final TalonFX m_deployMotor = new TalonFX(Constants.CanIds.kClimbDeployMotor);
+  private final TalonFX m_rotateMotor = new TalonFX(Constants.CanIds.kClimbRotateMotor);
+  private final DigitalInput m_inboardLimit = new DigitalInput(Constants.DioIds.kClimbInLimitSwitch);
+  private final DigitalInput m_outboardLimit = new DigitalInput(Constants.DioIds.kClimbOutLimitSwitch);
+  private final DigitalInput m_rotateCCWLimit = new DigitalInput(Constants.DioIds.kClimbCCWLimitSwitch);
+  private final DigitalInput m_rotateCWLimit = new DigitalInput(Constants.DioIds.kClimbCWLimitSwitch);
+
+  private final SysIdRoutine m_deploySysIdRoutine = new SysIdRoutine(
+      new SysIdRoutine.Config(
+          Units.Volts.per(Units.Second).of(0.5),
+          Units.Volts.of(2.0),
+          Units.Seconds.of(8.0)),
+      new SysIdRoutine.Mechanism(
+          (voltage) -> runDeploySysIdVolts(voltage.in(Units.Volts)),
+          (SysIdRoutineLog log) -> {
+            log.motor("climbDeploy")
+                .voltage(Units.Volts.of(m_deployMotor.get() * RobotController.getBatteryVoltage()))
+                .linearPosition(Units.Meters.of(getDeployDistanceM()))
+                .linearVelocity(Units.MetersPerSecond.of(getDeployVelocityDegPerSec()));
+          },
+          this));
+
+  private boolean m_enableDeployAutomation = false;
+  private boolean m_enableRotationAutomation = false;
+  private boolean m_deployEncoderSet = false;
+  private boolean m_rotateEncoderSet = false;
+  private double m_targetDeployDistanceM = 0.0;
+  private double m_targetRotationAngleDeg = Constants.Climb.kRotationInitialAngleDeg; // This could be different than the encoder-reset angle
+
+  /** Creates a new ClimbSub. */
+  public ClimbSub() {
+    TalonFXConfigurator talonFXConfiguratorDeploy = m_deployMotor.getConfigurator();
+    TalonFXConfigurator talonFXConfiguratorRotate = m_rotateMotor.getConfigurator();
+
+    // This is for the deploy
+    Slot0Configs slot0DeployConfigs = new Slot0Configs();
+    slot0DeployConfigs.kS = Constants.Climb.kDeployKS;
+    slot0DeployConfigs.kV = Constants.Climb.kDeployKV;
+    slot0DeployConfigs.kA = Constants.Climb.kDeployKA;
+    slot0DeployConfigs.kP = Constants.Climb.kDeployKP;
+    slot0DeployConfigs.kI = Constants.Climb.kDeployKI;
+    slot0DeployConfigs.kD = Constants.Climb.kDeployKD;
+    talonFXConfiguratorDeploy.apply(slot0DeployConfigs);
+
+    MotionMagicConfigs mmcDeploy = new MotionMagicConfigs();
+    mmcDeploy.MotionMagicCruiseVelocity = Constants.Climb.kDeployMaxVelocityMPerSec;
+    mmcDeploy.MotionMagicAcceleration = Constants.Climb.kDeployMaxAccelerationMPerSec;
+    talonFXConfiguratorDeploy.apply(mmcDeploy);
+
+    // This is for the rotate 
+    // TODO: Acoount for gravity in kRotateKG
+    Slot0Configs slot0RotateConfigs = new Slot0Configs();
+    slot0RotateConfigs.kS = Constants.Climb.kRotateKS;
+    slot0RotateConfigs.kV = Constants.Climb.kRotateKV;
+    slot0RotateConfigs.kA = Constants.Climb.kRotateKA;
+    slot0RotateConfigs.kP = Constants.Climb.kRotateKP;
+    slot0RotateConfigs.kI = Constants.Climb.kRotateKI;
+    slot0RotateConfigs.kD = Constants.Climb.kRotateKD;
+    talonFXConfiguratorRotate.apply(slot0RotateConfigs);
+
+    MotionMagicConfigs mmcRotate = new MotionMagicConfigs();
+    mmcRotate.MotionMagicCruiseVelocity = Constants.Climb.kRotateMaxVelocityMPerSec;
+    mmcRotate.MotionMagicAcceleration = Constants.Climb.kRotateMaxAccelerationMPerSec;
+    talonFXConfiguratorRotate.apply(mmcRotate);
+
+    // This is how you set a current limit inside the motor (vs on the input power supply)
+    CurrentLimitsConfigs limitConfigs = new CurrentLimitsConfigs();
+    limitConfigs.StatorCurrentLimit = Constants.Climb.kRotationMaxCurrent;
+    limitConfigs.StatorCurrentLimitEnable = true;
+    talonFXConfiguratorRotate.apply(limitConfigs);
+    limitConfigs.StatorCurrentLimit = Constants.Climb.kDeployMaxCurrent;
+    talonFXConfiguratorDeploy.apply(limitConfigs);
+
+    // This is how you can set a deadband, invert the motor rotoation and set brake/coast
+    MotorOutputConfigs outputConfigs = new MotorOutputConfigs();
+    outputConfigs.DutyCycleNeutralDeadband = 0.02; // Ignore values below 2%
+    outputConfigs.Inverted = InvertedValue.CounterClockwise_Positive; // Invert = Clockwise
+    outputConfigs.NeutralMode = NeutralModeValue.Brake;
+    talonFXConfiguratorRotate.apply(outputConfigs);
+    talonFXConfiguratorDeploy.apply(outputConfigs);
+  }
+
+  public void init() {
+    m_logger.info("Initializing ClimbSub Subsystem");
+    disableDeployAutomation();
+    disableRotateAutomation();
+    m_deployEncoderSet = false;
+    m_rotateEncoderSet = false;
+    setDeployPower(0.0);
+    setRotatePower(0.0);
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+    SmartDashboard.putBoolean("Cl Dep Auto", m_enableDeployAutomation);
+    SmartDashboard.putNumber("Cl Dep Target", m_targetDeployDistanceM);
+    SmartDashboard.putNumber("Cl Dep Dist", getDeployDistanceM());
+    SmartDashboard.putNumber("Cl Dep Power", m_deployMotor.get());
+    SmartDashboard.putBoolean("Cl Dep In", isAtDeployInLimit());
+    SmartDashboard.putBoolean("Cl Dep Out", isAtDeployOutLimit());
+
+    SmartDashboard.putBoolean("Cl Rot Auto", m_enableRotationAutomation);
+    SmartDashboard.putNumber("Cl Rot Target", m_targetRotationAngleDeg);
+    SmartDashboard.putNumber("Cl Rot Distance", getRotationAngleDeg());
+    SmartDashboard.putNumber("Cl Rot Power", m_rotateMotor.get());
+    SmartDashboard.putBoolean("Cl Rot CCW", isAtRotateCCWLimit());
+    SmartDashboard.putBoolean("Cl Rot CW", isAtRotateCWLimit());
+
+    // Reset Deploy Encoder If We Have Not Already
+    if(!m_deployEncoderSet && isAtDeployInLimit()) {
+      resetDeployEncoder();
+      m_deployEncoderSet = true;
+    }
+
+    // Reset Rotate Encoder If We Have Not Already
+    if(!m_rotateEncoderSet && isAtRotateCCWLimit()) {
+      resetRotateEncoder();
+      m_rotateEncoderSet = true;
+    }
+
+  }
+
+  public void setDeployPower(double power) {
+    m_deployMotor.set(power);
+  }
+
+  /**
+   * Manually set the power of the climb rotate motor.
+   * 
+   * @param power power value -1.0 to 1.0
+   */
+  public void setRotatePower(double power) {
+    m_rotateMotor.set(power);
+
+  }
+
+  public double getDeployDistanceM() {
+    return m_deployMotor.get();
+  }
+
+  public double getDeployVelocityDegPerSec() {
+    return m_deployMotor.getVelocity().getValueAsDouble();
+  }
+
+  /**
+   * Returns the current angular position of the climb arm
+   * 
+   * @return position in degrees
+   */
+  public double getRotationAngleDeg() {
+    return m_rotateMotor.getPosition().getValueAsDouble();
+  }
+
+  /**
+   * Sets the current angle as the zero angle
+   */
+  public void resetRotateEncoder() {
+    m_rotateMotor.setPosition(0.0);
+  }
+
+  public void resetDeployEncoder() {
+    m_deployMotor.setPosition(Constants.Climb.kDeployInDistanceM);
+  }
+
+  public boolean isAtDeployInLimit() {
+    return m_inboardLimit.get();
+  }
+
+  public boolean isAtDeployOutLimit() {
+    return m_outboardLimit.get();
+  }
+
+  public boolean isAtRotateCCWLimit() {
+    return m_rotateCCWLimit.get();
+  }
+
+  public boolean isAtRotateCWLimit() {
+    return m_rotateCWLimit.get();
+  }
+
+  ////////////////////////////// Deploy automation //////////////////////////////
+  public void enableDeployAutomation() {
+    // TODO: Convert Distance To Rotation if necessary
+    m_deployMotor.setControl(new MotionMagicTorqueCurrentFOC(m_targetDeployDistanceM).withSlot(0));
+    m_enableDeployAutomation = true;
+  }
+
+  public void disableDeployAutomation() {
+    m_enableDeployAutomation = false;
+    m_deployMotor.setControl(new DutyCycleOut(0.0));
+  }
+
+  public void setTargetDeployDistance(double distanceM) {
+    m_targetDeployDistanceM = distanceM;
+    enableDeployAutomation();
+  }
+
+  public boolean isAtTargetDistance() {
+    if((getDeployDistanceM() - m_targetDeployDistanceM) <= Constants.Climb.kDeployToleranceM) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  ////////////////////////////// Rotation automation //////////////////////////////
+  public void enableRotateAutomation() {
+    // TODO: Convert Angle to Rotations if necessary
+    m_enableRotationAutomation = true;
+    m_rotateMotor.setControl(new MotionMagicTorqueCurrentFOC(m_targetRotationAngleDeg).withSlot(0));
+  }
+
+  public void disableRotateAutomation() {
+    m_enableRotationAutomation = false;
+    m_rotateMotor.setControl(new DutyCycleOut(0.0));
+  }
+
+  public void setTargetRotateAngle(double angleDeg) {
+    m_targetRotationAngleDeg = angleDeg;
+    enableRotateAutomation();
+  }
+
+  public boolean isAtTargetRotateAngle() {
+    if((getRotationAngleDeg() - m_targetRotationAngleDeg) <= Constants.Climb.kRotationToleranceDeg) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  ////////////////////////////// Deploy SysId //////////////////////////////
+  public void runDeploySysIdVolts(double volts) {
+    MathUtil.clamp(volts, -(Constants.Climb.kDeployMaxPower * 12.0), (Constants.Climb.kDeployMaxPower * 12.0));
+    if(((volts < 0) && isAtDeployInLimit()) || (volts > 0) && isAtDeployOutLimit()) {
+      disableDeployAutomation();
+    }
+    if(isAtRotateCCWLimit() || isAtRotateCWLimit()) {
+      disableRotateAutomation();
+    }
+  }
+
+  public Command deploySysIdQuasistaticCmd(SysIdRoutine.Direction dir) {
+    return m_deploySysIdRoutine.quasistatic(dir);
+  }
+
+  public Command deploySysIdDynamicCmd(SysIdRoutine.Direction dir) {
+    return m_deploySysIdRoutine.dynamic(dir);
+  }
+}
---
+++ Test/ClimbTest/src/main/java/frc/robot/utils/ShooterAimingCalcs.java
@@ -0,0 +1,207 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.utils;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Transform2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
+import edu.wpi.first.math.interpolation.*;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.trajectory.Trajectory;
+import frc.robot.Constants;
+
+/** Add your docs here. */
+public class ShooterAimingCalcs {
+  private double targetYawAngle;
+  private double robotYawAngle;
+  private double relativeTargetYawAngle;
+
+  public double calculateShooterFlywheelRps(Pose2d robot) {
+    double rps;
+    InterpolatingDoubleTreeMap velocityInterpolation = new InterpolatingDoubleTreeMap();
+    //This will need to be derived experementally, currentlty based on theoretical values
+    velocityInterpolation.put(0.0, 0.0);
+    velocityInterpolation.put(1.0, 10.0);
+    velocityInterpolation.put(2.0, 10.0);
+    velocityInterpolation.put(3.0, 10.0);
+    velocityInterpolation.put(4.0, 11.0);
+    velocityInterpolation.put(5.0, 11.5);
+    velocityInterpolation.put(6.0, 12.0);
+    velocityInterpolation.put(7.0, 12.5);
+    velocityInterpolation.put(8.0, 13.0);
+    velocityInterpolation.put(9.0, 13.0);
+    velocityInterpolation.put(10.0, 13.0);
+    if(RobotStatus.isLobbing()) {
+      if(RobotStatus.getCurrentFieldPosition() == "RightLob") {
+        rps = velocityInterpolation.get(getDistanceFromRightLob(robot));
+      } else {
+        rps = velocityInterpolation.get(getDistanceFromLeftLob(robot));
+      }
+    } else if(RobotStatus.isShooting()) {
+      rps = velocityInterpolation.get(getDistanceFromHub(robot).getNorm());
+    } else {
+      if(RobotStatus.wasLobbing()) {
+        if(RobotStatus.getPreviousFieldPosition() == "RightLob") {
+          rps = velocityInterpolation.get(getDistanceFromRightLob(robot));
+        } else {
+          rps = velocityInterpolation.get(getDistanceFromLeftLob(robot));
+        }
+      } else if(RobotStatus.wasShooting()) {
+        rps = velocityInterpolation.get(getDistanceFromHub(robot).getNorm());
+      } else {
+        rps = 0.0;
+      }
+    }
+    return rps;
+  }
+
+  public double calculateShooterYawDegrees(Pose2d robot) {
+    robotYawAngle = robot.getRotation().getDegrees() + 180; //robot angle 0 to 360
+    targetYawAngle =
+        Math.toDegrees(Math.atan2(getDistanceFromHub(robot).getY(), getDistanceFromHub(robot).getX())) + 180;
+    relativeTargetYawAngle = (360 + (targetYawAngle - robotYawAngle)) % 360;
+    return relativeTargetYawAngle;
+  }
+
+  public double calculateTimeOfFlight(Pose2d robot) {
+    double distanceX = getDistanceFromHub(robot).getX();
+    double velocityX =
+        flywheelRpsToVelocity(calculateShooterFlywheelRps(robot))
+            * Math.cos(Math.toRadians(calculateShooterPitchDegrees(robot)[0]));
+    return distanceX / velocityX;
+  }
+
+  public double flywheelRpsToVelocity(double rps) {
+    return 0.0;
+  }
+
+  public Translation2d getDistanceFromHub(Pose2d robot) {
+    Alliance alliance = GameData.getAlliance();
+    Translation2d distToHub;
+    Pose2d turretPos = robot.plus(new Transform2d(-0.1, 0.1, new Rotation2d(0.0)));
+
+    if(alliance == Alliance.Blue) {
+      distToHub = new Translation2d(Constants.FieldElements.kBlueHubX - turretPos.getX(),
+          Constants.FieldElements.kBlueHubY - turretPos.getY());
+    } else {
+      distToHub = new Translation2d(Constants.FieldElements.kRedHubX - turretPos.getX(),
+          Constants.FieldElements.kRedHubY - turretPos.getY());
+    }
+    return distToHub;
+  }
+
+  public double getDistanceFromLeftLob(Pose2d robot) {
+    return Math.hypot(robot.getX() - Constants.TrajectoryCalculations.kLobX,
+        robot.getY() - Constants.TrajectoryCalculations.kLeftLobY);
+  }
+
+  public double getDistanceFromRightLob(Pose2d robot) {
+    return Math.hypot(robot.getX() - Constants.TrajectoryCalculations.kLobX,
+        robot.getY() - Constants.TrajectoryCalculations.kRightLobY);
+  }
+
+  public double[] calculateShooterPitchDegrees(Pose2d robot) {
+    // link to the visual trajectory calculation desmos graph
+    // desmos.com/calculator/kd3svmcurr
+    // link to the proof for each equation
+    // desmos.com/calculator/er6sltycq6
+    // 0 degrees output is horizontal, 90 is vertical pointing up
+
+    double dx;
+    double dy;
+    double g = Constants.Shooter.kGravity;
+    double v = flywheelRpsToVelocity(calculateShooterFlywheelRps(robot));
+    double pitch;
+
+    double uShoot = Constants.TrajectoryCalculations.interpolationShoot;
+    double uLob = Constants.TrajectoryCalculations.interpolationLobber;
+
+    if(RobotStatus.isLobbing()) {
+      if(RobotStatus.getCurrentFieldPosition() == "RightLob") {
+        dy = Constants.TrajectoryCalculations.kRightLobY;
+        dx = Constants.TrajectoryCalculations.kLobX;
+      } else {
+        dy = Constants.TrajectoryCalculations.kLeftLobY;
+        dx = Constants.TrajectoryCalculations.kLobX;
+      }
+    } else if(RobotStatus.isShooting()) {
+      dy = Constants.TrajectoryCalculations.shooterToHubHeight;
+      dx = Math.hypot(getDistanceFromHub(robot).getX(), getDistanceFromHub(robot).getY());
+    } else {
+      if(RobotStatus.wasLobbing()) {
+        if(RobotStatus.getPreviousFieldPosition() == "RightLob") {
+          dy = Constants.TrajectoryCalculations.kRightLobY;
+          dx = Constants.TrajectoryCalculations.kLobX;
+        } else {
+          dy = Constants.TrajectoryCalculations.kLeftLobY;
+          dx = Constants.TrajectoryCalculations.kLobX;
+        }
+      } else if(RobotStatus.wasShooting()) {
+        dy = Constants.TrajectoryCalculations.shooterToHubHeight;
+        dx = Math.hypot(getDistanceFromHub(robot).getX(), getDistanceFromHub(robot).getY());
+      } else {
+        dx = 0.0;
+        dy = 0.0;
+      }
+    }
+
+
+    double D = 1 - (2 * g * dy) / Math.pow(v, 2) - (Math.pow(g, 2) * Math.pow(dx, 2)) / Math.pow(v, 4);
+    double T1 = (Math.pow(v, 2) / (g * dx)) * (1 + Math.sqrt(D)); // Higher angle
+    double T2 = (Math.pow(v, 2) / (g * dx)) * (1 - Math.sqrt(D)); // Lower angle
+
+    double vMin = Math.sqrt(g * (dy + Math.sqrt(Math.pow(dx, 2) + Math.pow(dy, 2))));
+    double TMin = Math.pow(vMin, 2) / (g * dx);
+
+    double angleLow = Math.toDegrees(Math.atan(T2));
+    double angleHigh = Math.toDegrees(Math.atan(T1));
+    double angleInterpShoot = Math.toDegrees(Math.atan(uShoot * T1 + (1 - uShoot) * T2));
+    double angleInterpLob = Math.toDegrees(Math.atan(uLob * T1 + (1 - uLob) * T2));
+    double angleVMin = Math.toDegrees(Math.atan(TMin));
+
+    double finalVelocity = 0.0;
+
+    if(RobotStatus.isLobbing()) {
+      pitch = angleInterpLob;
+    } else if(RobotStatus.isShooting()) {
+      pitch =
+          (dx < Constants.TrajectoryCalculations.piecewiseSwapCalculationDistance) ? angleInterpShoot : angleVMin;
+      finalVelocity = (dx < Constants.TrajectoryCalculations.piecewiseSwapCalculationDistance) ? 0.0 : vMin;
+    } else {
+      pitch = 0.0;
+    }
+
+    if(D < 0) {
+      pitch = angleVMin;
+      finalVelocity = vMin;
+    }
+
+    double[] returns = {pitch, finalVelocity};
+    return returns;
+  }
+
+  public double[] calculationsInMotion(Pose2d robot, ChassisSpeeds velocity) {
+    double velocityX = velocity.vxMetersPerSecond;
+    double velocityY = velocity.vyMetersPerSecond;
+    double angularVelocityDegrees = Math.toDegrees(velocity.omegaRadiansPerSecond);
+    double offsetX = velocityX * calculateTimeOfFlight(robot); //realistically i dont see a better alternative to just using the current position to calculate the pitch of the shooter since it's necessary calculate the tof. It should be fine, since the value will be close enough to correct but we can always just add a fudge-facor based on our velocity in each direction 
+    double offsetY = velocityY * calculateTimeOfFlight(robot);
+    double offsetRot = angularVelocityDegrees * calculateTimeOfFlight(robot);
+    Pose2d offsetPos = new Pose2d(robot.getX() + offsetX, robot.getY() + offsetY,
+        robot.getRotation().plus(new Rotation2d(Math.toRadians(offsetRot))));
+    double[] pitchAndVelocity = calculateShooterPitchDegrees(offsetPos);
+    double[] trajectoriesArray = {pitchAndVelocity[0], calculateShooterYawDegrees(offsetPos),
+        pitchAndVelocity[1], offsetPos.getX(), offsetPos.getY(), offsetPos.getRotation().getDegrees()};
+    System.out.println(pitchAndVelocity[1]);
+    if(pitchAndVelocity[1] == 0.0) {
+      trajectoriesArray[2] = calculateShooterFlywheelRps(offsetPos);
+      System.out.println(trajectoriesArray[2]);
+    }
+
+    return trajectoriesArray;
+  }
+}
